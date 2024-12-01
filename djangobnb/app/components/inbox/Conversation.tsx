7f1d4c1be9a1061475905963f45090fec2c20d46
Zeltops <alexbellev@abv.bg>
2024-12-02 01:45:53 +0200

    2.1. Additional templates

---
+++ djangobnb/app/components/inbox/Conversation.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Conversation() {
+  return (
+    <div className='px-6 py-4 border border-gray-300 rounded-xl cursor-pointer'>
+        <p className='mb-6 text-xl'>John Doe</p>
+        <p className='text-airbnb-dark'>Go to conversation</p>
+    </div>
+  )
+}
+
+export default Conversation
